a4132e9c5ec65305ee270b55266e340150e25c31
analogy47 <actweed47@gmail.com>
2020-02-02 22:02:14 -0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
-import Footer from './components/Footer/Footer';
+import SignUp from './containers/SignUp/SignUp';
+import SignIn from './containers/SignIn/SignIn';
+
+const theme = createMuiTheme();
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <LandingPage />
-      <Footer />
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/login" component={SignIn} />
+            <Route exact path="/register" component={SignUp} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
     </div>
   );
 }
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import CallToAction from '../CallToAction/CallToAction';
 
 const LandingPage = () => {
   return (
     <div>
+      <Header />
       <Hero />
       <CallToAction />
+      <Footer />
     </div>
   );
 };
